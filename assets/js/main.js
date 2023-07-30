8a3f1c441d78d687dee36ed070442865dca67d16
SantanaDanilo <109323866+SantanaDanilo@users.noreply.github.com>
2023-07-30 00:33:15 -0300

    Primeiro envio do sorteador para testes

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+const nome = document.querySelector('.nomes__input')
+const enviar = document.querySelector('.nomes__btn')
+const lista = document.querySelector('.nomes__lista')
+const listaArr = []
+
+enviar.addEventListener('click', () => {
+    const linha = document.createElement('div')
+    const linhaLi = document.createElement('li')
+    const linhaBtn = document.createElement('button')
+    
+    linhaLi.innerText = nome.value
+    linhaBtn.innerText = 'X'
+
+    linha.appendChild(linhaLi)
+    linha.appendChild(linhaBtn)
+
+    linha.classList.add('lista__div')
+    linhaLi.classList.add('lista__linha')
+    linhaBtn.classList.add('lista__btn')
+
+    lista.appendChild(linha)
+    
+    listaArr.push(linhaLi.innerText)
+
+    nome.value = ""
+})
+
+
+
