9ee1868116801ccb986cf945cf657bae2bbec7e6
SantanaDanilo <109323866+SantanaDanilo@users.noreply.github.com>
2023-07-30 20:41:27 -0300

    Mais atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,19 @@
 const nome = document.querySelector('.nomes__input')
 const enviar = document.querySelector('.nomes__btn')
 const lista = document.querySelector('.nomes__lista')
+const times = document.querySelector('.times')
+const timeListaA = document.querySelector('.time__lista-a')
+const timeListaB = document.querySelector('.time__lista-b')
+const bancoLista = document.querySelector('.banco__lista')
+const timeArr = [timeListaA, timeListaB]
 const listaArr = []
 
 enviar.addEventListener('click', () => {
+    lista.classList.remove('nomes__lista--none')
+    lista.classList.add('nomes__lista')
+
+    times.style.display = 'none'
+
     const linha = document.createElement('div')
     const linhaLi = document.createElement('li')
     const linhaBtn = document.createElement('button')
@@ -23,6 +33,7 @@ enviar.addEventListener('click', () => {
     listaArr.push(linhaLi.innerText)
 
     nome.value = ""
+    nome.focus()
 
     linhaBtn.addEventListener('click', () => {
         if(listaArr.includes(linhaLi.innerText)) {
@@ -31,4 +42,41 @@ enviar.addEventListener('click', () => {
 
         linha.style.display = 'none'
     })
+})
+
+const sortear = document.querySelector('.sortear')
+
+sortear.addEventListener('click', () => {
+    lista.classList.remove('nomes__lista')
+    lista.classList.add('nomes__lista--none')
+    
+    times.style.display = 'inline'
+
+    function adicionaLista() {
+        for(let i = 0; i < listaArr.length; i++) {
+            const timeLi = document.createElement('li')
+    
+            timeLi.classList.add('time__nome')
+            timeLi.innerText = listaArr[parseInt(Math.random() * listaArr.length)]
+
+            const bancoLi = document.createElement('li')
+
+            bancoLi.classList.add('banco__linha')
+            bancoLi.innerText = timeLi.innerText
+
+            let listaAleatorio = parseInt(Math.random() * timeArr.length)
+    
+            if(timeListaA.childElementCount === 6 || listaAleatorio != 0) {
+                timeListaB.appendChild(timeLi)
+            } else if(timeListaB.childElementCount === 6 || listaAleatorio == 0) {
+                timeListaA.appendChild(timeLi)
+            }
+
+            if(listaArr.includes(timeLi.innerText)) {
+                listaArr.splice(listaArr.indexOf(timeLi.innerText), 1)
+            }
+        }
+    }
+
+    setInterval(adicionaLista, 1000)
 })
